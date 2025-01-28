7ed8a8adeef79bd688940d11a4ab4a25b42f6952
Zannie Johnson <zannethra@gmail.com>
2025-01-27 20:59:04 -0800

    Create dataset, fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.18.0"
     }
   }
 }
 
 provider "google" {
-  project     = "taxi-rides-ny-449202"
-  region      = "us-central1"
+  project = "taxi-rides-ny-449202"
+  region  = "us-central1"
 }
 
 resource "google_storage_bucket" "ny-taxi-data" {
   name          = "taxi-rides-ny-449202-ny-taxi-data"
-  location      = "US"
+  location      = var.gcp_location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +25,9 @@ resource "google_storage_bucket" "ny-taxi-data" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "taxi_dataset" {
+  dataset_id = var.bq_taxi_dataset
+  location   = var.gcp_location
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "gcp_location" {
+  default = "US"
+}
+
+variable "bq_taxi_dataset" {
+  default = "taxi_dataset"
+}
