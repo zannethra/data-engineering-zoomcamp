144a69885d736ebf6341bd090187d6619ed6d23a
Zannie Johnson <zannethra@gmail.com>
2025-01-27 20:28:24 -0800

    Make a bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,18 @@ provider "google" {
   project     = "taxi-rides-ny-449202"
   region      = "us-central1"
 }
+
+resource "google_storage_bucket" "ny-taxi-data" {
+  name          = "taxi-rides-ny-449202-ny-taxi-data"
+  location      = "US"
+  force_destroy = true
+  
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
